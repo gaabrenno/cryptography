df01bc4002d51e46fc9e4c223c244c5d18df3984
gaabrenno <gaabrenno@gmail.com>
2022-10-17 20:21:53 -0300

    apagando prints de teste

--- Cripto_APS_3.py
+++ Cripto_APS_3.py
@@ -16,14 +16,14 @@ def descript () : #função para descriptografar
     listaD = list() #Variavel que recebe uma lista
     for i in range(0, len(descriptografar), 6): #cria um array iniciando em 0 e indo até o sexto augaritimo de descriptografar
         listaD.append(descriptografar[i:i+6]) #insere dentro de listaD todos os números inseridos em descriptografar de 6 em 6 ['0123456', '0123456', '0123456'...]
-    print ('listaD',listaD)
+   
     listaI = list()
     for i in range(0, len(listaD)):
         result = ''
         for j in listaD[i]:
             result = result + j
         listaI.insert(i, chr(int(int(result)/14)))
-    print ('listaI', listaI)
+   
     for i in listaI:
         mensagem = mensagem + i
     
